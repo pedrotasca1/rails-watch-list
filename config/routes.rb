c7d7f656002fa4809ace61fc77ef28e6abef0af6
Pedro Tasca <pedrotasca1@gmail.com>
2023-02-24 08:48:07 +0000

    not complete

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+
+  resources :bookmarks
 end
